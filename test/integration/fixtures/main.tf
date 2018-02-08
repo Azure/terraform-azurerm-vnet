b35f53bf44484f6e87e7720d8698d8095d9bca7e
Richard Guthrie <richardguthrie@Richards-MBP.guest.corp.microsoft.com>
2018-02-07 17:01:05 -0800

    Adding support for associating a subnet and network security group

--- test/integration/fixtures/main.tf
+++ test/integration/fixtures/main.tf
@@ -1,13 +1,26 @@
 module "network" {
   source              = "../../../"
   resource_group_name = "myapp"
-  location            = "westus"
+  location            = "eastus"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
+  nsg_ids = {
+    subnet1 = "${azurerm_network_security_group.nsg1.id}"
+  }
   tags = {
     environment = "dev"
     costcenter  = "it"
   }
+}
+//Create a resource group and nsg to use for testing nsg association.
+resource "azurerm_resource_group" "myapp2" {
+  name     = "myapp2"
+  location = "eastus"
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                  = "nsg1"
+  resource_group_name   = "${azurerm_resource_group.myapp2.name}"
+  location              = "${azurerm_resource_group.myapp2.location}"
 }
