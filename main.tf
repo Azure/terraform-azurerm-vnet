13c34fca6e33df201c5c680b0491540e19eb66e9
shanoor <shanoor@someplace.com>
2019-01-29 10:57:05 +0100

    Removed resource group management by the module

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
 #Azure Generic vNet Module
-resource "azurerm_resource_group" "vnet" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.vnet_name}"
   location            = "${var.location}"
   address_space       = ["${var.address_space}"]
-  resource_group_name = "${azurerm_resource_group.vnet.name}"
+  resource_group_name = "${var.resource_group_name}"
   dns_servers         = "${var.dns_servers}"
   tags                = "${var.tags}"
 }
@@ -16,7 +11,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                      = "${var.subnet_names[count.index]}"
   virtual_network_name      = "${azurerm_virtual_network.vnet.name}"
-  resource_group_name       = "${azurerm_resource_group.vnet.name}"
+  resource_group_name       = "${var.resource_group_name}"
   address_prefix            = "${var.subnet_prefixes[count.index]}"
   service_endpoints         = "${var.subnet_service_endpoints[count.index]}"
   network_security_group_id = "${lookup(var.nsg_ids,var.subnet_names[count.index],"")}"
