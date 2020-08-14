aeb4ba5c92dad4ad52c20d922fe19713f22f269a
yupwei68 <yupwei@microsoft.com>
2020-08-14 16:01:17 +0800

    update

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = data.azurerm_resource_group.vnet.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_prefixes[count.index]]
-  service_endpoints    = lookup(var.subnet_service_endpoints, var.subnet_names, null)
+  service_endpoints    = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], null)
 }
 
 data "azurerm_subnet" "import" {
