55670f8adb653de4826aafbed4048267f5534614
Matheus Nunes <maths.nunes@gmail.com>
2021-02-18 02:56:57 -0300

    Allow configure enforce_private_link_endpoint_network_policies and enforce_private_link_service_network_policies params for subnets (#40)
    * Make possible enable/disable network policies for subnet
    
    * Add values to fixtures
    
    * Add examples, Use bool instead any
    
    * Add examples, Use bool instead any

--- main.tf
+++ main.tf
@@ -13,12 +13,14 @@ resource azurerm_virtual_network "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  count                = length(var.subnet_names)
-  name                 = var.subnet_names[count.index]
-  resource_group_name  = data.azurerm_resource_group.vnet.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.subnet_prefixes[count.index]]
-  service_endpoints    = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], null)
+  count                                          = length(var.subnet_names)
+  name                                           = var.subnet_names[count.index]
+  resource_group_name                            = data.azurerm_resource_group.vnet.name
+  virtual_network_name                           = azurerm_virtual_network.vnet.name
+  address_prefixes                               = [var.subnet_prefixes[count.index]]
+  service_endpoints                              = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], null)
+  enforce_private_link_endpoint_network_policies = lookup(var.subnet_enforce_private_link_endpoint_network_policies, var.subnet_names[count.index], false)
+  enforce_private_link_service_network_policies  = lookup(var.subnet_enforce_private_link_service_network_policies, var.subnet_names[count.index], false)
 }
 
 locals {
