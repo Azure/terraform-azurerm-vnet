cd0cd9b8a6f9473ed264637a7943677c301902f4
Andrew Hodgson <andrew.hodgson@contino.io>
2022-06-21 13:07:17 +0100

    Fix resource group circular dependency if vnet_location is specified

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 #Azure Generic vNet Module
 data "azurerm_resource_group" "vnet" {
-  name = var.resource_group_name
+  count = var.vnet_location != null ? 0 : 1
+  name  = var.resource_group_name
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  resource_group_name = data.azurerm_resource_group.vnet.name
-  location            = var.vnet_location != null ? var.vnet_location : data.azurerm_resource_group.vnet.location
+  resource_group_name = var.resource_group_name
+  location            = var.vnet_location != null ? var.vnet_location : data.azurerm_resource_group.vnet[0].location
   address_space       = var.address_space
   dns_servers         = var.dns_servers
   tags                = var.tags
@@ -15,7 +16,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   count                                          = length(var.subnet_names)
   name                                           = var.subnet_names[count.index]
-  resource_group_name                            = data.azurerm_resource_group.vnet.name
+  resource_group_name                            = var.resource_group_name
   virtual_network_name                           = azurerm_virtual_network.vnet.name
   address_prefixes                               = [var.subnet_prefixes[count.index]]
   service_endpoints                              = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], null)
