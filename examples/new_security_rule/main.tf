791f84e32ce4fae1bb1764d79ad583dba89b63c7
zjhe <hezijie@microsoft.com>
2022-10-09 18:06:28 +0800

    fix format issue

--- examples/new_security_rule/main.tf
+++ examples/new_security_rule/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "vnet" {
-  source              = "../../"
+  source = "../../"
 
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
