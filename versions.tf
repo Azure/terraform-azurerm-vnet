038bf854c3817da90718547148bf58c3216dc340
hezijie <lonegunmanb@hotmail.com>
2024-11-12 14:56:31 +0800

    bump azurerm provider to v4

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.2"
+  required_version = ">= 1.3.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.11, < 4.0"
+      version = "~> 4.0"
     }
   }
 }
